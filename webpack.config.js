5260f5e45d1a411561e006b8806965e8db47ee5d
Daniil Ponomarenko <den3free@gmail.com>
2024-12-19 05:31:01 +0300

    Установит и настроит сборщик (webpack)

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ]
+};
