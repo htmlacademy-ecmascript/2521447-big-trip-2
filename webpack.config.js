a957ac5ac0b7a4862550d5539d50c33df4edb91f
Daniil Ponomarenko <den3free@gmail.com>
2024-12-19 05:50:56 +0300

    Установит и настроит Babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,19 @@ module.exports = {
         },
       ],
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ],
+  },
 };
