ae560eddb3edd4cb63898b68fc6b9dd0ab310338
Daniil Ponomarenko <den3free@gmail.com>
2025-01-18 09:16:32 +0300

    Выделяет показ точки маршрута в функцию. Скрывает отображение формы редактирования. WIP

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -1,7 +1,7 @@
 import { render } from '../framework/render.js';
 import PointsView from '../view/points-view.js';
 import SortView from '../view/sort-view.js';
-import PointEditView from '../view/point-edit-view.js';
+// import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 
 
@@ -28,23 +28,26 @@ export default class TripEventsPresenter {
     this.#point = this.#points[0];
     render(new SortView(), this.#pointsContainer);
     render(this.#pointsComponent, this.#pointsContainer);
-    render(new PointEditView({
-      point: this.#point,
-      types: this.#offersModel.getTypes(),
-      destination: this.#destinationsModel.getDestinationById(this.#point.destination),
-      offers: this.#offersModel.getOffersByType(this.#point.type),
-      checkedList: this.#offersModel.getOffersSelected(this.#point),
-    }), this.#pointsComponent.element);
+    // render(new PointEditView({
+    //   point: this.#point,
+    //   types: this.#offersModel.getTypes(),
+    //   destination: this.#destinationsModel.getDestinationById(this.#point.destination),
+    //   offers: this.#offersModel.getOffersByType(this.#point.type),
+    //   checkedList: this.#offersModel.getOffersSelected(this.#point),
+    // }), this.#pointsComponent.element);
 
     for (let i = 0; i < this.#points.length; i++) {
-      const destination = this.#destinationsModel.getDestinationById(this.#points[i].destination);
-      const offers = this.#offersModel.getOffersSelected(this.#points[i]);
+      const point = this.#points[i];
+      const destination = this.#destinationsModel.getDestinationById(point.destination);
+      const offers = this.#offersModel.getOffersSelected(point);
 
-      render(new PointView({
-        point: this.#points[i],
-        destination: destination,
-        offers: offers,
-      }), this.#pointsComponent.element);
+      this.#renderPoint(point, destination, offers);
     }
+  }
+
+  #renderPoint(point, destination, offers) {
+    const pointComponent = new PointView({ point, destination, offers });
+
+    render(pointComponent, this.#pointsComponent.element);
   }
 }
